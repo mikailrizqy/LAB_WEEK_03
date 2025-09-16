79610dfbf4f7c8878c3487b1e7ae76e7eafc0b11
Mikail Rizqy <m1kailr0505@gmail.com>
2025-09-16 22:48:15 +0700

    Commit No. 3 use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -6,28 +6,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.util.Log
+import androidx.fragment.app.FragmentContainerView
 
 
 interface CoffeeListener {
     fun onSelected(id: Int)
 }
-class MainActivity : AppCompatActivity(), CoffeeListener{
+
+class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v,
-                                                                             insets ->
+
+        // Apply window insets to root layout (LinearLayout or ConstraintLayout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
-                systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ListFragment())
+                .commit()
+        }
     }
-    override fun onSelected(id: Int){
-        val detailFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_detail)
-                as DetailFragment
-        detailFragment.setCoffeeData(id)
+
+    override fun onSelected(id: Int) {
+        // Jika kamu ingin mengganti detailFragment ketika item dipilih:
+        val detailFragment = DetailFragment.newInstance(id)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, detailFragment) // ganti di fragment_detail
+            .addToBackStack(null)
+            .commit()
     }
 }
